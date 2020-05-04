5debe1849dd2ce6e27ba87911670be83de34d4d4
catalunha <catalunha.mj@gmail.com>
2020-05-04 11:16:43 -0300

    aula103

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import logo from '../../assets/react.svg'
+import './styles.css'
 
 function Header() {
   return (
-    <div>
-      Header
-    </div>
+    <header className='container'>
+      <Link to='/'>
+      <img className='logo' src={logo} alt='Logo do projeto'/>
+      </Link>
+      <Link to='/reservas' className='reserva'>
+        <div>
+          <strong>Minhas reservas</strong>
+          <span>0 reservas</span>
+        </div>
+      </Link>
+    </header>
   )
 }
 export default Header
