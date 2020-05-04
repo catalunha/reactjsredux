1504e6681ac9ed3aa5e64b24e3c8cf9573927e5e
catalunha <catalunha.mj@gmail.com>
2020-05-04 17:09:01 -0300

    aula115

--- src/store/modules/reserve/reducer.js
+++ src/store/modules/reserve/reducer.js
@@ -7,15 +7,16 @@ function reserve(state = [], action) {
   switch (action.type) {
     case 'ADD_RESERVE_SUCESS':
       return produce(state, draft => {
-        const tripIndex = draft.findIndex(trip => trip.id === action.trip.id)
-        if (tripIndex >= 0) {
-          draft[tripIndex].amount += 1
-        } else {
-          draft.push({
-            ...action.trip,
-            amount: 1,
-          })
-        }
+        // const tripIndex = draft.findIndex(trip => trip.id === action.trip.id)
+        // if (tripIndex >= 0) {
+        //   draft[tripIndex].amount += 1
+        // } else {
+        //   draft.push({
+        //     ...action.trip,
+        //     amount: 1,
+        //   })
+        // }
+        draft.push(action.trip)
       })
     case 'REMOVE_RESERVE':
       return produce(state, draft => {
