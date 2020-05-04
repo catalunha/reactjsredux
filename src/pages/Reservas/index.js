87c37b4c3a855bb581707c7adf394eb64b322928
catalunha <catalunha.mj@gmail.com>
2020-05-04 13:26:47 -0300

    aula106

--- src/pages/Reservas/index.js
+++ src/pages/Reservas/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { MdDelete } from 'react-icons/md'
 
+import './styles.css'
 function Reservas() {
   return (
     <div>
-     Reservas2
+      <h1 className='title'>Você solicitou 1 reserva</h1>
+      <div className='reservas'> 
+        <img
+          src='https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg'
+          alt='Maceio'
+        />
+        <strong>Viagem Maceio 7 dias</strong>
+        <span>Quantidade: 2</span>
+        <button
+          type='button'
+          onClick={() => { }}
+        >
+          <MdDelete size={20} color='#191919' />
+        </button>
+        
+      </div>
+      <footer>
+          <button
+            type='button'
+          >
+            Solicitar reservas
+         </button>
+        </footer>
     </div>
   );
 }
