037a086d3899fcd1f1e577e0f15a2b840c83c75a
catalunha <catalunha.mj@gmail.com>
2020-05-04 11:36:58 -0300

    aula104

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api'
 function Home() {
+  const [trips, setTrips] = useState([])
+
+  useEffect(() => {
+    async function loadApi() {
+      const response = await api.get('trips')
+      setTrips(response.data)
+      console.log(response.data)
+    }
+    loadApi()
+  }, [])
   return (
     <div>
-     Home
+      Home
     </div>
   );
 }
